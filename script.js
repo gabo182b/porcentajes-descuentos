70eb9e00992387d095201d25fefc58326ca122bd
Gabriel <flexflit18@gmail.com>
2021-11-22 13:39:48 -0400

    añadi la funcionalidad de añadir cupones en HTML y JavaScript

--- script.js
+++ script.js
@@ -5,4 +5,33 @@ function discountedPriceCalculator(price, discount) {
   return discountedPrice;
 }
 
-console.log(discountedPriceCalculator(3000, 12));
+function cuponCheck(cupon) {
+  const cuponsOptions = {
+    Silver: 10,
+    Gold: 25,
+    Diamond: 50,
+  };
+
+  const cuponDiscount = cuponsOptions[cupon] || 0;
+
+  return cuponDiscount;
+}
+
+function priceCalculator() {
+  const inputPrice = document.getElementById("inputPrice");
+  const price = inputPrice.value;
+  const inputDiscount = document.getElementById("inputDiscount");
+  const discount = inputDiscount.value;
+  const inputCupon = document.getElementById("discount-cupon");
+  const cupon = inputCupon.value;
+
+  const discountedPrice = discountedPriceCalculator(price, discount);
+
+  const cuponDiscount = cuponCheck(cupon);
+
+  const newPrice = discountedPrice - cuponDiscount;
+
+  const newTotalPrice = document.getElementById("newPrice");
+
+  newTotalPrice.innerText = newPrice + " $";
+}
